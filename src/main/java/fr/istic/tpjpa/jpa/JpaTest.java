2e50e3024c82b7aac4205d4ce5054a4387e0ffd9
StephaneMangin <stephane.mangin@freesbee.fr>
2015-01-20 14:31:32 +0100

    Initial commit

---
+++ src/main/java/fr/istic/tpjpa/jpa/JpaTest.java
@@ -0,0 +1,39 @@
+package fr.istic.tpjpa.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaTest {
+
+	private EntityManager manager;
+
+	public JpaTest(EntityManager manager) {
+		this.manager = manager;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("example");
+		EntityManager manager = factory.createEntityManager();
+		JpaTest test = new JpaTest(manager);
+
+		EntityTransaction tx = manager.getTransaction();
+		tx.begin();
+
+		// TODO create entity
+
+		// TODO persist entity
+
+		tx.commit();
+
+		// TODO run request
+
+		System.out.println(".. done");
+	}
+
+}
