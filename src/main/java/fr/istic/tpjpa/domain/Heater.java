16b3ebd31c653c3de9c4c1b48b20e4798297b69f
StephaneMangin <stephane.mangin@freesbee.fr>
2015-02-03 16:00:48 +0100

    Using SINGLE_TABLE inheritancy for AbstractDevice

--- src/main/java/fr/istic/tpjpa/domain/Heater.java
+++ src/main/java/fr/istic/tpjpa/domain/Heater.java
@@ -2,48 +2,7 @@ package fr.istic.tpjpa.domain;
 import javax.persistence.*;
 
 @Entity
-public class Heater implements DeviceInterface {
+@DiscriminatorValue("Heater")
+public class Heater extends AbstractDevice {
 	
-	private long id;
-	private Home home;
-	private int wattHeure;
-
-    public Heater() {
-		super();
-	}
-
-	@Id
-	@GeneratedValue
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-    @ManyToOne
-    public Home getHome() {
-		return home;
-	}
-    /** 
-	 * Modifie le propriétaire
-	 * 
-	 * Process de mise à jour:
-	 * Vérifier que ce n'est pas une suppression de propriétaire
-	 * Vérifie que le propriétaire actuel n'est pas déjà le même que le nouveau
-	 * Faire l'appel récursif sur l'ajout d'un chauffage au propriétaire
-	 * 
-	 * @param person
-	 */
-	public void setHome(Home home) {
-		if (home != null && home != this.home) {
-			this.home = home;
-			home.addHeatr(this);
-		}
-	}
-	public int getWattHeure() {
-		return wattHeure;
-	}
-	public void setWattHeure(int wattHeure) {
-		this.wattHeure = wattHeure;
-	}
 }
