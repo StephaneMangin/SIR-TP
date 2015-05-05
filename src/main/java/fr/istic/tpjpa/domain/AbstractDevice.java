93b40178980c4fbb4d42f23a5df16d88d699e0e9
StéphaneMangin <stephane.mangin@freesbee.fr>
2015-05-05 11:03:18 +0200

    Merge resolving

--- src/main/java/fr/istic/tpjpa/domain/AbstractDevice.java
+++ src/main/java/fr/istic/tpjpa/domain/AbstractDevice.java
@@ -1,77 +1,4 @@
-<<<<<<< HEAD
 package fr.istic.tpjpa.domain;
-
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorType;
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.ManyToOne;
-
-@Entity
-@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="type",discriminatorType=DiscriminatorType.STRING)
-@DiscriminatorValue("AbstractDevice")
-public abstract class AbstractDevice {
-	
-	private long id;
-	private String name;
-	private Home home;
-	private int wattHeure;
-
-	@Id
-	@GeneratedValue
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-    @ManyToOne
-    public Home getHome() {
-		return home;
-	}
-    
-    /** 
-	 * Modifie le propriétaire
-	 * 
-	 * Process de mise à jour:
-	 * Vérifier que ce n'est pas une suppression de propriétaire
-	 * Vérifie que le propriétaire actuel n'est pas déjà le même que le nouveau
-	 * Faire l'appel récursif sur l'ajout d'un device au propriétaire
-	 * 
-	 * @param person
-	 */
-	public void setHome(Home home) {
-		if (home != null && home != this.home) {
-			this.home = home;
-			home.addDevice(this);
-		}
-	}
-	public int getWattHeure() {
-		return wattHeure;
-	}
-	public void setWattHeure(int wattHeure) {
-		this.wattHeure = wattHeure;
-	}
-
-}
-=======
-package fr.istic.tpjpa.domain;
-
-import org.codehaus.jackson.annotate.JsonBackReference;
-import org.codehaus.jackson.annotate.JsonManagedReference;
 
 import java.io.Serializable;
 
@@ -124,4 +51,3 @@ public abstract class AbstractDevice implements Serializable {
 	}
 
 }
->>>>>>> 8f0d621... Bugfix JPA accessors
--- src/main/java/fr/istic/tpjpa/jpa/JpaTest.java
+++ src/main/java/fr/istic/tpjpa/jpa/JpaTest.java
@@ -40,21 +40,15 @@ public class JpaTest {
 			for (int j = 0; j<numHeaters; j++) {
 				int wattheure = rand.nextInt(10000 - 2000 + 1) + 2000;
 				Heater heater = new Heater();
-<<<<<<< HEAD
-=======
 				heater.setName("Heater " + j);
->>>>>>> 8f0d621... Bugfix JPA accessors
 				heater.setWattHeure(wattheure);
-				home.addHeatr(heater);
+				home.addDevice(heater);
 				manager.persist(heater);
 			}
 			for (int z = 0; z<numElecDevice; z++) {
 				int wattheure = rand.nextInt(2500 - 200 + 1) + 200;
 				ElectronicDevice device = new ElectronicDevice();
-<<<<<<< HEAD
-=======
 				device.setName("Electronic device " + z);
->>>>>>> 8f0d621... Bugfix JPA accessors
 				device.setWattHeure(wattheure);
 				home.addDevice(device);
 				manager.persist(device);
@@ -70,15 +64,6 @@ public class JpaTest {
 	@SuppressWarnings("deprecation")
 	public void createPersons(){
 		int numOfPersons = manager.createQuery("SELECT a FROM Person a", Person.class).getResultList().size();
-<<<<<<< HEAD
-		// Flush is done for data insertion mechanism purposes
-		if (numOfPersons != 0) {
-			manager.createQuery("DROP TABLE ElectronicDevice");
-			manager.createQuery("DROP TABLE Heater");
-			manager.createQuery("DROP TABLE Home");
-			manager.createQuery("DROP TABLE person_friends");
-			manager.createQuery("DROP TABLE Person");
-=======
 		if (numOfPersons == 0) {
 			// Personne 1
 			Person person1 = new Person();
@@ -142,70 +127,7 @@ public class JpaTest {
 			person2.addFriend(person5);
 			person4.addFriend(person1);
 			person5.addFriend(person1);
->>>>>>> 8f0d621... Bugfix JPA accessors
 		}
-		// Personne 1
-		Person person1 = new Person();
-		person1.setName("Robert");
-		person1.setFirstname("Julien");
-		person1.setEmail("julien.robert@gmail.com");
-		person1.setGender("Masculin");
-		person1.setFacebookProfile(person1.getName()+"."+person1.getFirstname());
-		person1.setBirthday(new Date(10, 02, 1972));
-		manager.persist(person1);
-		createHome(person1, 1, 3, 9);
-		
-		// Personne 2
-		Person person2 = new Person();
-		person2.setName("Marie");
-		person2.setFirstname("Julie");
-		person2.setEmail("julie.marie@gmail.com");
-		person2.setGender("Feminin");
-		person2.setFacebookProfile(person2.getName()+"."+person2.getFirstname());
-		person2.setBirthday(new Date(03, 12, 1950));
-		manager.persist(person2);
-		createHome(person2, 1, 8, 6);
-
-		// Personne 3
-		Person person3 = new Person();
-		person3.setName("Marcel");
-		person3.setFirstname("Lucien");
-		person3.setEmail("lucien.marcel@gmail.com");
-		person3.setGender("Masculin");
-		person3.setFacebookProfile(person3.getName()+"."+person3.getFirstname());
-		person3.setBirthday(new Date(12, 06, 1974));
-		manager.persist(person3);
-		createHome(person3, 1, 12, 23);
-
-		// Personne 4
-		Person person4 = new Person();
-		person4.setName("Beaufort");
-		person4.setFirstname("Yvan");
-		person4.setEmail("yvan.beaufort@yahoo.fr");
-		person4.setGender("Masculin");
-		person4.setFacebookProfile(person4.getName()+"."+person4.getFirstname());
-		person4.setBirthday(new Date(10, 02, 2015));
-		manager.persist(person4);
-		createHome(person4, 1, 4, 7);
-
-		// Personne 5
-		Person person5 = new Person();
-		person5.setName("Martin");
-		person5.setFirstname("Delphine");
-		person5.setEmail("delphine.martin@orange.fr");
-		person5.setGender("Feminin");
-		person5.setFacebookProfile(person5.getName()+"."+person5.getFirstname());
-		person5.setBirthday(new Date(05, 03, 1990));
-		manager.persist(person5);
-		createHome(person5, 1, 3, 9);
-		
-		// Attribution des amis
-		person1.addFriend(person2);
-		person1.addFriend(person3);
-		person2.addFriend(person3);
-		person2.addFriend(person5);
-		person4.addFriend(person1);
-		person5.addFriend(person1);
 	}
 
 	private void listPersons() {
